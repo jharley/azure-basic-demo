82e6ace2d2bbf308e48ffa3517dacc9d74d4a1c4
Fraser Pollock <fraserpol@gmail.com>
2018-08-17 12:01:51 -0400

    Output values

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "vm_public_ip" {
-  value       = "${azurerm_public_ip.myterraformpublicip.ipaddress}"
+  value       = "${azurerm_public_ip.myterraformpublicip.ip_address}"
   description = "The Demo VPC Name for later use"
 }
